5f1fefb891a0be0f17d6832aa1e54b005fd18723
YSRKEN <ysr.ken@gmail.com>
2018-03-07 12:05:47 +0900

    Viewにおける各コントロールのプロパティにBindingを付けていく作業をした

--- KFC/KFC/MainViewModel.cs
+++ KFC/KFC/MainViewModel.cs
@@ -3,6 +3,7 @@ using Eto.Forms;
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
@@ -18,10 +19,31 @@ namespace KFC
 		public ReactiveProperty<Image> PreviewImage { get; } = new ReactiveProperty<Image>();
 		public ReadOnlyReactiveProperty<bool> PreviewImageFlg1 { get; }
 		public ReadOnlyReactiveProperty<bool> PreviewImageFlg2 { get; }
+		public ReactiveProperty<bool> ShowImageDataFlg1 { get; } = new ReactiveProperty<bool>(true);
+		public ReactiveProperty<bool> ShowImageDataFlg2 { get; } = new ReactiveProperty<bool>(true);
+		public ReactiveProperty<bool> ShowImageDataFlg3 { get; } = new ReactiveProperty<bool>(true);
 
 		// コマンド
 		public ReactiveCommand CreateFormationPictureCommand { get; } = new ReactiveCommand();
 		public ReactiveCommand DeleteDataAllCommand { get; } = new ReactiveCommand();
+		//
+		public ReactiveCommand AddPiecePicture1Command { get; } = new ReactiveCommand();
+		public ReactiveCommand AddPiecePicture2Command { get; } = new ReactiveCommand();
+		public ReactiveCommand AddPiecePicture3Command { get; } = new ReactiveCommand();
+		public ReactiveCommand DeletePiecePicture1Command { get; } = new ReactiveCommand();
+		public ReactiveCommand DeletePiecePicture2Command { get; } = new ReactiveCommand();
+		public ReactiveCommand DeletePiecePicture3Command { get; } = new ReactiveCommand();
+		public ReactiveCommand MoveUpPiecePicture1Command { get; } = new ReactiveCommand();
+		public ReactiveCommand MoveUpPiecePicture2Command { get; } = new ReactiveCommand();
+		public ReactiveCommand MoveUpPiecePicture3Command { get; } = new ReactiveCommand();
+		public ReactiveCommand MoveDownPiecePicture1Command { get; } = new ReactiveCommand();
+		public ReactiveCommand MoveDownPiecePicture2Command { get; } = new ReactiveCommand();
+		public ReactiveCommand MoveDownPiecePicture3Command { get; } = new ReactiveCommand();
+
+		// コレクション
+		public ReactiveCollection<string> PiecePictureList1 { get; } = new ReactiveCollection<string>();
+		public ReactiveCollection<string> PiecePictureList2 { get; } = new ReactiveCollection<string>();
+		public ReactiveCollection<string> PiecePictureList3 { get; } = new ReactiveCollection<string>();
 
 		public void SetPreViewImage(Image image) {
 			PreviewImage.Value = image;
@@ -34,6 +56,18 @@ namespace KFC
 			//
 			CreateFormationPictureCommand.Subscribe(_ => { MessageBox.Show("画像作成"); });
 			DeleteDataAllCommand.Subscribe(_ => { MessageBox.Show("全消去"); });
+			AddPiecePicture1Command.Subscribe(_ => { MessageBox.Show("画像追加"); });
+			AddPiecePicture2Command.Subscribe(_ => { MessageBox.Show("画像追加"); });
+			AddPiecePicture3Command.Subscribe(_ => { MessageBox.Show("画像追加"); });
+			DeletePiecePicture1Command.Subscribe(_ => { MessageBox.Show("画像削除"); });
+			DeletePiecePicture2Command.Subscribe(_ => { MessageBox.Show("画像削除"); });
+			DeletePiecePicture3Command.Subscribe(_ => { MessageBox.Show("画像削除"); });
+			MoveUpPiecePicture1Command.Subscribe(_ => { MessageBox.Show("上移動"); });
+			MoveUpPiecePicture2Command.Subscribe(_ => { MessageBox.Show("上移動"); });
+			MoveUpPiecePicture3Command.Subscribe(_ => { MessageBox.Show("上移動"); });
+			MoveDownPiecePicture1Command.Subscribe(_ => { MessageBox.Show("下移動"); });
+			MoveDownPiecePicture2Command.Subscribe(_ => { MessageBox.Show("下移動"); });
+			MoveDownPiecePicture3Command.Subscribe(_ => { MessageBox.Show("下移動"); });
 		}
 	}
 }
